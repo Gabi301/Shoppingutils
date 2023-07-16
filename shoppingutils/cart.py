8330f6cb1b3ef845ea031b8c8f1c46eaa6ac1c49
Gabi301 <108729223+Gabi301@users.noreply.github.com>
2023-07-16 16:28:06 +0100

    Update cart.py

--- shoppingutils/cart.py
+++ shoppingutils/cart.py
@@ -1,5 +1,5 @@
 def calculate_total_price(cart):
     itens = produtos["items"]
     precos = itens[1::2]  # LISTAR APENAS OS PREÇOS
-    valor_total = sum(precos)
-    return valor_total
+    cart = sum(precos)
+    return cart
